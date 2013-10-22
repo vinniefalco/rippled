f1ddf16e4bf6646847e802e1d4da8b483a454875
Patrick Dehne <patrick@mysonicweb.de>
2013-10-22 23:22:50 +0200

    VFLib to Beast migration module beast_vflib

---
+++ src/beast/modules/beast_vflib/threads/ThreadWithServiceQueue.cpp
@@ -0,0 +1,194 @@
+//------------------------------------------------------------------------------
+/*
+	This file is part of Beast: https://github.com/vinniefalco/Beast
+	Copyright 2013, Vinnie Falco <vinnie.falco@gmail.com>
+
+	Permission to use, copy, modify, and/or distribute this software for any
+	purpose  with  or without fee is hereby granted, provided that the above
+	copyright notice and this permission notice appear in all copies.
+
+	THE  SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+	WITH  REGARD  TO  THIS  SOFTWARE  INCLUDING  ALL  IMPLIED  WARRANTIES  OF
+	MERCHANTABILITY  AND  FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+	ANY  SPECIAL ,  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+	WHATSOEVER  RESULTING  FROM  LOSS  OF USE, DATA OR PROFITS, WHETHER IN AN
+	ACTION  OF  CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+	OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+//==============================================================================
+
+#include "ThreadWithServiceQueue.h"
+
+namespace beast {
+
+ThreadWithServiceQueue::ThreadWithServiceQueue (const String& name)
+: Thread (name)
+, m_entryPoints(nullptr)
+, m_calledStart(false)
+, m_calledStop(false)
+{
+}
+
+ThreadWithServiceQueue::~ThreadWithServiceQueue()
+{
+	stop(true);
+}
+
+void ThreadWithServiceQueue::start (EntryPoints* const entryPoints)
+{
+	{
+		CriticalSection::ScopedLockType lock (m_mutex);
+		
+		// start() MUST be called.
+		bassert (!m_calledStart);
+		m_calledStart = true;
+	}
+	
+	m_entryPoints = entryPoints;
+	
+	startThread();
+}
+
+void ThreadWithServiceQueue::stop (bool const wait)
+{
+	{
+		CriticalSection::ScopedLockType lock (m_mutex);
+		
+		// start() MUST be called.
+		bassert (m_calledStart);
+		
+		if (!m_calledStop)
+		{
+			m_calledStop = true;
+			
+			{
+				CriticalSection::ScopedUnlockType unlock (m_mutex);
+				
+				call (&Thread::signalThreadShouldExit, this);
+				
+				// something could slip in here
+				
+				// m_queue.close();
+			}
+		}
+	}
+	
+	if (wait)
+		waitForThreadToExit();
+}
+
+void ThreadWithServiceQueue::run ()
+{
+	m_entryPoints->threadInit();
+	
+	while (! this->threadShouldExit())
+	{
+		m_entryPoints->threadIdle();
+		run_one();
+	}
+	
+	m_entryPoints->threadExit();
+}
+
+//------------------------------------------------------------------------------
+
+namespace detail
+{
+	
+//------------------------------------------------------------------------------
+
+class BindableServiceQueueTests
+: public UnitTest
+{
+public:
+	
+	struct BindableServiceQueueRunner
+	: public ThreadWithServiceQueue::EntryPoints
+	{
+		ThreadWithServiceQueue m_worker;
+		int cCallCount, c1CallCount;
+		
+		BindableServiceQueueRunner()
+		: m_worker("BindableServiceQueueRunner")
+		, cCallCount(0)
+		, c1CallCount(0)
+		{
+		}
+		
+		void start()
+		{
+			m_worker.start(this);
+		}
+		
+		void stop()
+		{
+			m_worker.stop(true);
+		}
+		
+		void c()
+		{
+			m_worker.queue(&BindableServiceQueueRunner::cImpl, this);
+		}
+		
+		void cImpl()
+		{
+			cCallCount++;
+		}
+		
+		void c1(int p1)
+		{
+			m_worker.queue(&BindableServiceQueueRunner::c1Impl, this, p1);
+		}
+		
+		void c1Impl(int p1)
+		{
+			c1CallCount++;
+		}
+	};
+		
+	static int const calls = 10000;
+	
+	void performCalls()
+	{
+		beginTestCase("perform calls");
+		
+		Random r;
+		r.setSeedRandomly();
+		
+		BindableServiceQueueRunner runner;
+		
+		runner.start();
+		
+		for(std::size_t i=0; i<calls; i++)
+		{
+			int wait = r.nextLargeNumber(10).toInteger();
+			
+			if(wait % 2)
+				runner.c();
+			else
+				runner.c1Impl(wait);
+		}
+
+		runner.stop();
+		
+		expect ((runner.cCallCount + runner.c1CallCount) == calls);
+		
+		pass();
+	}
+	
+	void runTest()
+	{
+		performCalls ();
+	}
+	
+	BindableServiceQueueTests () : UnitTest ("BindableServiceQueue", "beast")
+	{
+	}
+};
+
+static BindableServiceQueueTests bindableServiceQueueTests;
+	
+}
+
+
+}
